9e5ecd94a2f8ba882682fe95762f0542b8e4d715
Sebastien Beal <sebastibe@gmail.com>
2013-02-24 00:03:05 +0900

    Remove HTTP Basic Authentication

--- api/settings.py
+++ api/settings.py
@@ -176,7 +176,6 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.XMLParser',
         ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
         ),
