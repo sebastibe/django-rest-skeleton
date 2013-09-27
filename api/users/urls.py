dbd5d78ef15f89816f0631a2cedcda8c97efd727
Sebastien Beal <sebastibe@gmail.com>
2013-09-27 17:07:52 +0900

    Remove trailing slash in users / groups urls

--- api/users/urls.py
+++ api/users/urls.py
@@ -3,7 +3,8 @@ from rest_framework import routers
 
 from .views import UserViewSet, GroupViewSet
 
-router = routers.SimpleRouter()
+router = routers.SimpleRouter(trailing_slash=False)
+
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 
